aa583d554f314151e47c6b768b10927b56a4cea1
zhenquan dai <zhenquan.dai@uisee.com>
2024-02-29 11:46:30 +0800

    版本更新

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"text/template"
 
 	"github.com/iokr/coder/core/filex"
 	"github.com/iokr/coder/core/golang"
 )
 
-var dirPath = flag.String("dir", "./CodeStudy", "please input dir path")
+var dirPath = flag.String("dir", "./CodeStudy/src", "please input dir path")
+
+const (
+	destDirPath = "./CodeStudy/dest"
+)
 
 func main() {
 	flag.Parse()
@@ -25,33 +30,36 @@ func main() {
 		panic(err)
 	}
 
-	err = getAllFiles(tmpl, *dirPath)
+	err = getAllFiles(tmpl, *dirPath, destDirPath, 0)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func getAllFiles(tmpl *template.Template, dirPath string) error {
+func getAllFiles(tmpl *template.Template, srcDirPath, destDirPath string, dirLevel int) error {
 	// 获取目录下所有文件和子目录
-	entries, err := os.ReadDir(dirPath)
+	entries, err := os.ReadDir(srcDirPath)
 	if err != nil {
 		return err
 	}
 
 	for _, entry := range entries {
-		fullPath := filepath.Join(dirPath, entry.Name())
+		srcFullPath := filepath.Join(srcDirPath, entry.Name())
+		destFullPath := filepath.Join(destDirPath, entry.Name())
 		if entry.IsDir() {
+			// 目标文件夹创建目录
+			if err := os.Mkdir(destFullPath, 0777); err != nil {
+				return err
+			}
+
 			// 如果是子目录，则递归获取子目录下的文件
-			if err := getAllFiles(tmpl, fullPath); err != nil {
+			currentDirLevel := dirLevel + 1
+			if err := getAllFiles(tmpl, srcFullPath, destFullPath, currentDirLevel); err != nil {
 				return err
 			}
 		} else {
 			// 如果是文件，则进行文件内容转换
-			if err := convertToHTMLFile(tmpl, entry.Name(), fullPath); err != nil {
-				return err
-			}
-
-			if err := os.Remove(fullPath); err != nil {
+			if err := convertToHTMLFile(tmpl, entry.Name(), srcFullPath, destFullPath, dirLevel); err != nil {
 				return err
 			}
 		}
@@ -59,20 +67,27 @@ func getAllFiles(tmpl *template.Template, dirPath string) error {
 	return nil
 }
 
-func convertToHTMLFile(tmpl *template.Template, fileName, filePath string) error {
-	contentByte, err := filex.ReadAll(filePath)
+func convertToHTMLFile(tmpl *template.Template, fileName, srcFilePath, destFilePath string, dirLevel int) error {
+	contentByte, err := filex.ReadAll(srcFilePath)
 	if err != nil {
 		return err
 	}
 
-	fd, err := filex.CreateIfNotExist(fmt.Sprintf("%s.html", filePath))
+	fd, err := filex.CreateIfNotExist(fmt.Sprintf("%s.html", destFilePath))
 	if err != nil {
 		return err
 	}
 	defer fd.Close()
 
+	staticPath := make([]string, 0, dirLevel)
+	for i := 0; i < dirLevel; i++ {
+		staticPath = append(staticPath, "..")
+	}
+
+	staticFullPath := strings.Join(staticPath, "/")
 	return tmpl.Execute(fd, map[string]string{
 		"fileName":    fileName,
 		"codeContent": golang.FormatCode(string(contentByte)),
+		"staticPath":  staticFullPath,
 	})
 }
